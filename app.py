3aa639a5dd04726fab490c1aa294cc2edfb33e1d
Hanan Fokkens <hananfokkens@gmail.com>
2020-07-20 23:48:33 +1200

    Improved Code and added Comments

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ api_url = 'http://localhost:8080/api/'
 
 auth = BearerAuth('admin', 'admin', api_url + 'auth/login-form')
 
-r = requests.get(api_url + '/api/users', auth=auth)
+r = requests.get(api_url + '/api/users', auth=auth)  # make a new request using the bearer auth
--- bearer_auth.py
+++ bearer_auth.py
@@ -2,16 +2,16 @@ import requests
 
 
 class BearerAuth(requests.auth.AuthBase):
-    def __init__(self, username, password, auth_url, retries=3):
+    def __init__(self, username, password, auth_url):
         self.username = username
         self.password = password
         self.auth_url = auth_url
-        self.token = ''
-        self.retries = retries
+        self._token = ''
 
     def handel_401(self, response, **kwargs):
         """Handel a token/auth error."""
         if response.status_code == 401:  # this may need to be changed to include all 4xx errors
+            # make a post request to get the token
             token_r = requests.post(self.auth_url, params={
                 'grant_type': 'password',
                 'username': self.username,
@@ -20,14 +20,18 @@ class BearerAuth(requests.auth.AuthBase):
                 'Content-Type': 'application/x-www-form-urlencoded'
             })
             if token_r.status_code == 200:
-                self.token = token_r.json()['access_token']  # update the token
+                # if the getting the token was successful update it
+                self._token = token_r.json()['access_token']  # update the token
 
-            new_request = response.request.copy()
-            new_request.headers['Authorization'] = "Bearer %s" % self.token
+            new_request = response.request.copy()  # create a copy of the user request
+            new_request.headers['Authorization'] = "Bearer %s" % self._token  # update the Auth header
+            # return a new response made from the modified request
             return response.connection.send(new_request, **kwargs)
         return response
 
     def __call__(self, request):
-        request.headers['Authorization'] = "Bearer %s" % self.token
+        # add the Authorization header
+        request.headers['Authorization'] = "Bearer %s" % self._token
+        # add a hook to check if there was a 401 error and try again
         request.register_hook('response', self.handel_401)
         return request  # return the request
